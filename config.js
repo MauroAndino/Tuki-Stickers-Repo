79f3249b229ab6102d0f7721b6c80139b35b2dec
Mauro <mauroandino49@gmail.com>
2026-04-13 00:02:30 -0300

    Configurar sincronizacion remota

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.TUKI_REMOTE_CONFIG = {
-  enabled: false,
+  enabled: true,
   supabaseUrl: "https://mlqchbuuaayvyiuarguk.supabase.co",
   supabaseAnonKey: "sb_publishable_3dubllonutn7hJphMP-v2w_Rps0Z9Rv",
   stateRowId: "main",
